5557da060a72d27d6a383bc691fb98d4c0d65da0
Damien Duignan <damien.duignan1@gmail.com>
2015-11-16 21:23:56 +0000

    Text Updates

--- js/controllers/ProjectsController.js
+++ js/controllers/ProjectsController.js
@@ -4,28 +4,36 @@ app.controller('ProjectsController', ['$scope', function($scope){
     $scope.tech = [
 		{
 			name: 'Django',
-			description: 'Django is an open source MVC framework written and developed in Python. ' +
-			'It comes wth a lot of built in features to aid in the set up of your web application.'
+			description: [
+				'Django is an open source MVC framework written and developed in Python. ' +
+				'It comes wth a lot of built in features to aid in the set up of your web application.'
+			]
 		},
 		{
 			name: 'AngularJS',
-			description: 'AngularJS is an open source MVC JavaScript Framework maintained by Google. ' +
-			'It provides client-side MVC architectures, which aid in the development of single page applications.'
+			description: [
+				'AngularJS is an open source MVC JavaScript Framework maintained by Google. ' +
+				'It provides client-side MVC architectures, which aid in the development of single page applications.'
+			]
 		},
 		{
 			name: 'Bootstrap',
-			description: 'Bootstrap is a responsive design framework built by Twitter. It simplifies the ' +
-			'creation of responsive websites.'
+			description: [
+				'Bootstrap is a responsive design framework built by Twitter. It simplifies the ' +
+				'creation of responsive websites.',
+			]
 		}
 	];
 
     $scope.projects = [
 		{
 			name: 'Attendance App',
-			description: 'For my final year project I created an Android application which allows the ' +
-			'parent to track their children\'s progress in national or secondary school. ' +
-			'The application provides a real-time representation of the progress being made by the student.' +
-			'The system utilizes technologies such as MySQL, Apache HTTP Server, PHP and JSON'
+			description: [
+				'For my final year project I created an Android application which allows the ' +
+				'parent to track their children\'s progress in national or secondary school. ' +
+				'The application provides a real-time representation of the progress being made by the student.' +
+				'The system utilizes technologies such as MySQL, Apache HTTP Server, PHP and JSON'
+			]
 		}
 	];
 }]);
--- js/controllers/XPController.js
+++ js/controllers/XPController.js
@@ -11,9 +11,10 @@ app.controller('XPController', ['$scope', function($scope){
             endDate: 'Present',
             description: [
                 'airG is a Canadian software company headquartered in Vancouver, British Columbia. Its products include telecom software, a social networking service designed for mobile use, and a suite of multiplayer mobile games and applications.',
-                'I am part of the Irish branch of airG. As a team, we are responsible for the integration and maintenance of our products and services across the EMEA and Africa region.',
-                'I am involved in the design, implementation, and end to end testing of the integrations.',
-                'In addition to working on larger projects for a period of time, I am regularly working with our Business Solutions team to improve our services by implementing and sometimes designing new features.'
+                'As part of the airG Ireland team, I am responsible for the integration and maintenance of our products and services across the EMEA and Africa region.',
+                'The main priority is integrations. A typical integration would involve gathering requirements from our clients, design and planning stage, ' +
+                'implementation, and lastly, end to end testing.',
+                'In addition to working on larger integrations projects, I am regularly making changes to improve our services by implementing and sometimes designing new features.'
             ]
         },
         {
@@ -27,7 +28,10 @@ app.controller('XPController', ['$scope', function($scope){
             endDate: new Date(2012, 05, 30),
             description: [
                 'Kingspan is a global leader in high performance insulation, building fabric, and solar integrated building envelopes.',
-                'My main responsibility was to create internal web applications for Kingspan. I was using C#, ASP.NET,HTML and SQL Server to create web pages and applications. For one of these websites, I created a CMS from scratch so that the page could be updated easily without changing the source code.'
+                'My main responsibility was to create internal web applications for Kingspan. I was using C#, ASP.NET, HTML and SQL Server ' +
+                'to create web pages and applications.',
+                'For one of these websites, I created a Content Management System from scratch so that' +
+                ' the page could be updated easily without changing the source code.'
             ]
         },
         {
